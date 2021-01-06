9e1e3051fceb124535f5f71b15d1582e2a0ae7da
ioschpmi <michael.ioschpa@student.uni-augsburg.de>
2021-01-06 15:21:34 +0100

    added move method for automatic movement

--- logic.py
+++ logic.py
@@ -9,6 +9,7 @@ import random
 
 import constants as c
 
+
 #######
 # Task 1a #
 #######
@@ -28,6 +29,7 @@ def new_game(n):
     matrix = add_two(matrix)
     return matrix
 
+
 ###########
 # Task 1b #
 ###########
@@ -39,13 +41,14 @@ def new_game(n):
 
 
 def add_two(mat):
-    a = random.randint(0, len(mat)-1)
-    b = random.randint(0, len(mat)-1)
+    a = random.randint(0, len(mat) - 1)
+    b = random.randint(0, len(mat) - 1)
     while mat[a][b] != 0:
-        a = random.randint(0, len(mat)-1)
-        b = random.randint(0, len(mat)-1)
+        a = random.randint(0, len(mat) - 1)
+        b = random.randint(0, len(mat) - 1)
     mat[a][b] = 2
     return mat
+
 
 ###########
 # Task 1c #
@@ -72,19 +75,20 @@ def game_state(mat):
             if mat[i][j] == 0:
                 return 'not over'
     # check for same cells that touch each other
-    for i in range(len(mat)-1):
+    for i in range(len(mat) - 1):
         # intentionally reduced to check the row on the right and below
         # more elegant to use exceptions but most likely this will be their solution
-        for j in range(len(mat[0])-1):
-            if mat[i][j] == mat[i+1][j] or mat[i][j+1] == mat[i][j]:
+        for j in range(len(mat[0]) - 1):
+            if mat[i][j] == mat[i + 1][j] or mat[i][j + 1] == mat[i][j]:
                 return 'not over'
-    for k in range(len(mat)-1):  # to check the left/right entries on the last row
-        if mat[len(mat)-1][k] == mat[len(mat)-1][k+1]:
+    for k in range(len(mat) - 1):  # to check the left/right entries on the last row
+        if mat[len(mat) - 1][k] == mat[len(mat) - 1][k + 1]:
             return 'not over'
-    for j in range(len(mat)-1):  # check up/down entries on last column
-        if mat[j][len(mat)-1] == mat[j+1][len(mat)-1]:
+    for j in range(len(mat) - 1):  # check up/down entries on last column
+        if mat[j][len(mat) - 1] == mat[j + 1][len(mat) - 1]:
             return 'not over'
     return 'lose'
+
 
 ###########
 # Task 2a #
@@ -102,8 +106,9 @@ def reverse(mat):
     for i in range(len(mat)):
         new.append([])
         for j in range(len(mat[0])):
-            new[i].append(mat[i][len(mat[0])-j-1])
+            new[i].append(mat[i][len(mat[0]) - j - 1])
     return new
+
 
 ###########
 # Task 2b #
@@ -123,6 +128,7 @@ def transpose(mat):
         for j in range(len(mat)):
             new[i].append(mat[j][i])
     return new
+
 
 ##########
 # Task 3 #
@@ -159,10 +165,10 @@ def cover_up(mat):
 
 def merge(mat, done):
     for i in range(c.GRID_LEN):
-        for j in range(c.GRID_LEN-1):
-            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
+        for j in range(c.GRID_LEN - 1):
+            if mat[i][j] == mat[i][j + 1] and mat[i][j] != 0:
                 mat[i][j] *= 2
-                mat[i][j+1] = 0
+                mat[i][j + 1] = 0
                 done = True
     return mat, done
 
@@ -206,3 +212,15 @@ def right(game):
     game = cover_up(game)[0]
     game = reverse(game)
     return game, done
+
+
+def move(game, dir):
+    if dir == "up":
+        up(game)
+    elif dir == "down":
+        down(game)
+    elif dir == "left":
+        left(game)
+    else:
+        right(game)
+
